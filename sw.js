79223921e18b1a5167cdc024379bdd7102c111a9
Georgi Piskov <georgipiskov@gmail.com>
2026-01-28 18:38:43 +0200

    v3.6: Update Service Worker cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Rodopi Dent PWA
-const CACHE_VERSION = 'v1.1.0';
+const CACHE_VERSION = 'v3.6.0';
 const CACHE_NAME = `rodopi-dent-${CACHE_VERSION}`;
 
 // Files to cache for offline use
